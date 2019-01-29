d967937111688701b9633b47632ec611d4647ec4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-01-29 21:05:28 +0100

    build: initial wip converting aio examples to bazel

--- aio/content/examples/ajs-quick-reference/src/app/app.module.ts
+++ aio/content/examples/ajs-quick-reference/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { MovieListComponent } from './movie-list.component';
 import { AppRoutingModule } from './app-routing.module';
+import { StringSafeDatePipe } from './date.pipe';
 
 @NgModule({
   imports: [
@@ -15,7 +16,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   declarations: [
     AppComponent,
-    MovieListComponent
+    MovieListComponent,
+    StringSafeDatePipe
   ],
   bootstrap: [ AppComponent ]
 })
